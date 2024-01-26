489e0bbfd2d509772681183466ef9ac2cade4c43
Seifi <serafim.proj@gmail.com>
2024-01-26 23:08:52 +0100

    SERVICE: class CustomerService add methods

--- shtrans-service/src/main/java/at/shtrans/service/CustomerService.java
+++ shtrans-service/src/main/java/at/shtrans/service/CustomerService.java
@@ -1,5 +1,6 @@
 package at.shtrans.service;
 
+import at.shtrans.dto.CustomerDTO;
 import at.shtrans.repository.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -10,5 +11,23 @@ public class CustomerService {
     @Autowired
     private CustomerRepository customerRepository;
 
+    public CustomerDTO save(CustomerDTO customerDTO){
+        //throw new NotImplementedException("save");
+        return null;
+    }
 
+    public CustomerDTO update(CustomerDTO customerDTO){
+        //throw new NotImplementedException("update");
+        return null;
+    }
+
+    public Long delete(CustomerDTO customerDTO){
+        //throw new NotImplementedException("delete");
+        return null;
+    }
+
+    public Long deleteById(Long customerId){
+        //throw new NotImplementedException("customerId");
+        return null;
+    }
 }
