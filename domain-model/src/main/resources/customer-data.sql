ac2b3a65ecfb596ed6f69f678b873541dcaba534
Seifi <serafim.proj@gmail.com>
2024-02-01 19:32:51 +0100

    DOMAIN: add Database configuration

---
+++ domain-model/src/main/resources/customer-data.sql
@@ -0,0 +1,5 @@
+INSERT INTO customer (id, version, first_name, last_name) VALUES (1, 0, 'Ivan', 'Ivanov');
+INSERT INTO customer (id, version, first_name, last_name) VALUES (2, 0, 'Petar', 'Petrov');
+INSERT INTO customer (id, version, first_name, last_name) VALUES (3, 0, 'Vasil', 'Vasilev');
+INSERT INTO customer (id, version, first_name, last_name) VALUES (4, 0, 'Stoicho', 'Stoev');
+INSERT INTO customer (id, version, first_name, last_name) VALUES (5, 0, 'Valko', 'Valkov');
