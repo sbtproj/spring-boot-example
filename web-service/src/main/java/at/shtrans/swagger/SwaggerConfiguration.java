6e7342d5601e90cd6a58b40627a91254330daf5c
Seifi <serafim.proj@gmail.com>
2024-02-03 21:34:09 +0100

    SWAGGER: refactoring swagger configuration

--- web-service/src/main/java/at/shtrans/swagger/SwaggerConfiguration.java
+++ web-service/src/main/java/at/shtrans/swagger/SwaggerConfiguration.java
@@ -1,33 +1,56 @@
 package at.shtrans.swagger;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
-//import io.swagger.v3.oas.models.info.Info;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
 import io.swagger.v3.oas.models.servers.Server;
-
+import io.swagger.v3.oas.models.info.Info;
 import java.util.List;
 
 @Configuration
-//@OpenAPIDefinition(info = @Info(title = "Employees API", version = "2.0", description = "Employees Information" ))
 public class SwaggerConfiguration {
+
+    @Value("${project.version}")
+    private String projectVersion;
+
+    @Value("${swagger.open.api.info.title}")
+    private String infoTitle;
+
+    @Value("${swagger.open.api.info.description}")
+    private String infoDescription;
+
+    @Value("${swagger.open.api.info.contact.email}")
+    private String contactEmail;
+
+    @Value("${swagger.open.api.info.contact.name}")
+    private String contactName;
+
+    @Value("${swagger.open.api.info.contact.url}")
+    private String contactUrl;
+
+    @Value("${swagger.open.api.info.server1.url}")
+    private String applicationUrl;
+
+    @Value("${swagger.open.api.info.server1.description}")
+    private String description;
 
     @Bean
     public OpenAPI openAPI() {
-
-        Server devServer = new Server();
-        devServer.setUrl("http://localhost:8080/web-service-1.0-SNAPSHOT/api");
-        devServer.setDescription("Server URL in Development environment");
-
-        return new OpenAPI()
-                .info(new io.swagger.v3.oas.models.info.Info().title("the title").version("0.0").description("My API")
-                        .contact(new Contact().name("Fred").url("http://gigantic-server.com").email("Fred@gigagantic-server.com")))
-                .servers(List.of(devServer));
+        return new OpenAPI().info(buildInfo()).servers(List.of(buildServer()));
     }
-    // https://github.com/eugenp/tutorials/blob/master/spring-boot-modules/spring-boot-swagger-springfox/src/main/java/com/baeldung/swagger2/SwaggerConfig.java
+
+    private Info buildInfo() {
+        return new Info().title(infoTitle).version(projectVersion).description(infoDescription).contact(buildContact());
+    }
+
+    private Contact buildContact() {
+        return new Contact().email(contactEmail).name(contactName).url(contactUrl);
+    }
+
+    private Server buildServer(){
+        return new Server().url(applicationUrl).description(description);
+    }
 }
 
