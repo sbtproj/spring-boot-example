9d07ac9813667db97329cca4c682b7114d1855ef
Seifi <serafim.proj@gmail.com>
2024-02-04 17:00:02 +0100

    REPOSITORY: add new methods to CustomerRepository

--- repository/src/main/java/at/shtrans/repository/CustomerRepository.java
+++ repository/src/main/java/at/shtrans/repository/CustomerRepository.java
@@ -5,8 +5,16 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface CustomerRepository
         extends JpaRepository<Customer, Long>, JpaSpecificationExecutor<Customer> {
 
+    List<Customer> findByFirstName(String firstName);
+
+    List<Customer> findByLastName(String lastName);
+
+    List<Customer> findByVersion(Integer version);
 }
