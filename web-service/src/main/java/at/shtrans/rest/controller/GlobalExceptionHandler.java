e9f9dd594604d10ae67dd7a06a88130cf476e716
Seifi <serafim.proj@gmail.com>
2024-02-12 21:27:36 +0100

    WEB-SERVICE: add GlobalExceptionHandler

---
+++ web-service/src/main/java/at/shtrans/rest/controller/GlobalExceptionHandler.java
@@ -0,0 +1,8 @@
+package at.shtrans.rest.controller;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+}
+// https://medium.com/@aedemirsen/spring-boot-global-exception-handler-842d7143cf2a
