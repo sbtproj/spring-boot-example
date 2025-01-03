a696fd8c8775d04b8cbc64c723a8ddc85b4ca198
Seifi <serafim.proj@gmail.com>
2025-01-03 14:25:46 +0100

    REPOSITORY: comment ProjectionRepository

---
+++ repository/src/main/java/at/shtrans/repository/CustomerProjectionRepository.java
@@ -0,0 +1,19 @@
+/*package at.shtrans.repository;
+
+import at.shtrans.domain.Customer;
+import at.shtrans.projection.CustomerProjection;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Component;
+import th.co.geniustree.springdata.jpa.repository.JpaSpecificationExecutorWithProjection;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface CustomerProjectionRepository
+        extends JpaRepository<Customer, Long>, JpaSpecificationExecutorWithProjection<Customer, Long> {
+
+    List<CustomerProjection> findByFirstName(String firstName);
+
+}
+ */
---
+++ repository/src/test/java/at/shtrans/repository/CustomerProjectionRepositoryTest.java
@@ -0,0 +1,32 @@
+/*
+package at.shtrans.repository;
+
+import at.shtrans.projection.CustomerProjection;
+import com.github.database.rider.core.api.dataset.DataSet;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class CustomerProjectionRepositoryTest {
+
+    @Autowired
+    private CustomerProjectionRepository customerProjectionRepository;
+
+    @Test
+    @DisplayName("Single test successful")
+    @DataSet(value = "datasets/customer/empty.yml")
+    public void shouldSeedFromDeclaredConnection() {
+
+        List<CustomerProjection> projectionList =  customerProjectionRepository.findByFirstName("Ivan");
+
+
+       // assertNotNull(customerRepository.count());
+      //  assertEquals(0, customerRepository.count());
+    }
+}
+ */
