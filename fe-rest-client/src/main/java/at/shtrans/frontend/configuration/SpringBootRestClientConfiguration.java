082f1862bf62b3462e769976c9e10d12df4c3193
Seifi <serafim.proj@gmail.com>
2025-01-13 16:14:19 +0100

    REST-CLIENT: create module fe-rest-client

---
+++ fe-rest-client/src/main/java/at/shtrans/frontend/configuration/SpringBootRestClientConfiguration.java
@@ -0,0 +1,41 @@
+package at.shtrans.frontend.configuration;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.web.client.RestClient;
+
+import java.util.Base64;
+
+@Configuration
+@ComponentScan(basePackages = {"at.shtrans.frontend.api.service"})
+@PropertySource("classpath:client-application.properties")
+public class SpringBootRestClientConfiguration {
+
+    @Value("${restful.service.uri:http://localhost:8080/restful-ws/}")
+    private String apiUrl;
+
+    @Value("${restful.service.auth.user}")
+    private String user;
+
+    @Value("${restful.service.auth.password}")
+    private String password;
+
+    @Bean
+    public RestClient restClient() {
+        return RestClient
+                .builder()
+                .baseUrl(apiUrl)
+            //    .defaultHeader(HttpHeaders.AUTHORIZATION, encodeBasic(user, password))
+                .build();
+    }
+
+    private String encodeBasic(String username, String password) {
+        return "Basic "+ Base64
+                .getEncoder()
+                .encodeToString((username+":"+password).getBytes());
+    }
+}
