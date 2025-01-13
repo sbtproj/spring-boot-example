47ac899d7bdb964f1f273ac211322c6d539e86fb
Seifi <serafim.proj@gmail.com>
2025-01-13 13:49:01 +0100

    FE-REST-CLIENT: add logger

--- frontend-jsf/src/main/java/at/shtrans/frontend/api/service/CustomerApiService.java
+++ frontend-jsf/src/main/java/at/shtrans/frontend/api/service/CustomerApiService.java
@@ -1,45 +1,66 @@
 package at.shtrans.frontend.api.service;
 
 import at.shtrans.frontend.model.Customer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClient;
-
-import org.springframework.http.MediaType;
 
 import java.util.List;
 
 @Service
 public class CustomerApiService {
 
+    private final static Logger LOGGER = LoggerFactory.getLogger(CustomerApiService.class);
+
     @Autowired
     private RestClient restClient;
 
-    String url ="customer/";
+    String url = "customer/";
 
     public Customer create(Customer customer) {
-        return restClient.post() // Angeben, dass dies eine GET-Anfrage ist
+        LOGGER.info("BEGIN : create -> {}", customer);
+
+        customer = restClient.post() // Angeben, dass dies eine GET-Anfrage ist
                 .uri(url + "/create") // URI für die Anfrage festlegen
                 .accept(MediaType.APPLICATION_JSON)
+                .body(customer)
                 .retrieve() // Anfrage ausführen und Antwort abrufen
                 .body(Customer.class); // Antworttext als Customer extrahieren
+
+        LOGGER.info("END : create -> {}", customer);
+        return customer;
     }
 
     public Customer getById(Long id) {
-        return restClient.get() // Angeben, dass dies eine GET-Anfrage ist
-                .uri(url + "/{id}",id) // URI für die Anfrage festlegen
+        LOGGER.info("BEGIN : getById -> id={}", id);
+
+        Customer customer = restClient.get() // Angeben, dass dies eine GET-Anfrage ist
+                .uri(url + "/{id}", id) // URI für die Anfrage festlegen
                 .accept(MediaType.APPLICATION_JSON)
                 .retrieve() // Anfrage ausführen und Antwort abrufen
                 .body(Customer.class); // Antworttext als Customer extrahieren
+
+        LOGGER.info("END : getById -> {}", customer);
+        return customer;
     }
 
     public List<Customer> getAll() {
-        return restClient.get() // Angeben, dass dies eine GET-Anfrage ist
+        LOGGER.info("BEGIN : getAll");
+
+        List<Customer> customerList = restClient.get() // Angeben, dass dies eine GET-Anfrage ist
                 .uri(url) // URI für die Anfrage
                 .accept(MediaType.APPLICATION_JSON)
                 .retrieve() // Anfrage ausführen und Antwort abrufen
-                .body(new ParameterizedTypeReference<List<Customer>>() {}); // Antworttext als Customer extrahieren
+                .body(new ParameterizedTypeReference<List<Customer>>() {
+                }); // Antworttext als Customer extrahieren
+
+
+        LOGGER.info("END : getAll -> {}", customerList);
+        return customerList;
     }
 
 }
